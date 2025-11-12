47598cd932bede08d071bd768575a9f0e26ce92c
mzxyz <8177474+mzxyz@users.noreply.github.com>
2025-11-13 11:08:00 +1300

    Init `Aussie` RN app

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  root: true,
+  env: {
+    'react-native/react-native': true,
+    es6: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-native',
+    'react-hooks',
+    'import',
+    'prettier',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier', // Always last
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  rules: {
+    // Imports
+    'import/order': [
+      'warn',
+      {
+        groups: ['@screens', '@components', '@utils', '@types', '@hooks', '@stores', '@api'],
+        'newlines-between': 'always',
+      },
+    ],
+  },
+};
